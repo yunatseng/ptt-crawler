1c7b412f08686d26e9e998fe012bae1a13c75f0a
yunaTseng <fav0124@gmail.com>
2023-01-20 16:59:20 +0800

    FIX: sth went wrong

--- examples/utils.js
+++ examples/utils.js
@@ -2,13 +2,16 @@ const { MongoClient } = require('mongodb');
 
 /** 轉成貼文列表 Post[] e.g. [{ title: '...', url: '...' }, ...] */
 function parse2Posts(result = {}) {
+	// console.log(result);  // { titles: [], urls: [], ..., contents: [] }
 	const extractType = (title = '') => {
 		const results = /\[(.*?)\]/g.exec(title);
 		return (results && results[1]) || null;
 	};
 	const extractDetail = (text = '') => {
+		// console.log(text);
 		const timeStampRegExp = /([a-zA-Z]{3}\s.*\s\d{2}\s\d{2}:\d{2}:\d{2}\s\d{4})/;
-		const date = timeStampRegExp.exec(text)[1];
+		const result = timeStampRegExp.exec(text);
+		const date = (result && result[1]) || null;
 		const cleanText = text
 			.replace(/[\n\r]/g, '')
 			.replace(new RegExp(`作者.*看板.*標題.*時間${timeStampRegExp.source}`), '');
@@ -17,18 +20,19 @@ function parse2Posts(result = {}) {
 		return {
 			content,
 			comments: `※${comments}`,
-			date,
+			date: new Date(date),
 		};
 	};
 
 	const posts = [];
 	Object.entries(result).forEach(([key, values]) => {
-		const newKey = key.slice(0, -1);
+		const newKey = key.slice(0, -1); // titles -> title
 		values.forEach((value = '', index = 0) => {
 			posts[index] = {
 				...posts[index],
 				[newKey]: value,
 
+				// [type] ...
 				...(newKey === 'title' ? { type: extractType(value) } : {}),
 				...(newKey === 'content' ? extractDetail(value): {})
 			};
--- examples/write2db.js
+++ examples/write2db.js
@@ -24,17 +24,17 @@ async function main() {
 
 	// 初始爬蟲
 	await pttCrawler.initialize({
-		headless: false,
+		headless: true,
 	});
 
 	try {
 		/** 保險版 */
 		const board = 'Insurance';
 		/** 頁數 */
-		const pages = 3;
+		const pages = 5;
 		console.log(`開始爬 ${board} 版，共 ${pages} 頁`);
 
-		/** 爬蟲結果 */
+		/** 爬蟲結果 e.g. { titles[], urls[], rates[], authors[], dates[], marks[], contents[] } */
 		const result = await pttCrawler.getResults({
 			board: board,
 			pages: pages,
@@ -42,7 +42,7 @@ async function main() {
 			getContents: true
 		});
 		
-		// 轉成貼文列表
+		// 轉成貼文列表 e.g. [{ title: '...', url: '...' }, ...]
 		const posts = parse2Posts(result);
 		console.log(`爬完 ${board} 版 ${pages} 頁，結果共 ${posts.length} 篇`, /* posts */);
 
@@ -52,8 +52,13 @@ async function main() {
 		const filteredPosts = filterPosts({ posts, keywords });
 		console.log(`原 ${posts.length} 篇，根據 ${keywords.join('、')} 過濾完後剩 ${filteredPosts.length} 篇`);
 	
-		const res = await collection.insertMany(filteredPosts);
-		console.log(`寫入 ${dbName}/${collectionName} 表成功`, res);
+		if (filteredPosts.length) {
+			const res = await collection.insertMany(filteredPosts);
+			console.log(`寫入 ${dbName}/${collectionName} 表成功`, res);
+		}
+		else 	console.log(`因為過濾完後剩 ${filteredPosts.length} 篇故不寫入 DB`);
+		
+		
 	} catch (error) {
 		console.error('爬 PTT 失敗', error);
 	}
